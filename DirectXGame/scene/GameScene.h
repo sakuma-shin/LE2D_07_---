3712fcf1c3413fcf63385728648d476d12ecbf6a
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-04-17 14:35:41 +0900

    1-3確認課題

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -7,6 +7,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -39,10 +40,31 @@ public: // メンバ関数
 	/// </summary>
 	void Draw();
 
+
 private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	/*uint32_t modelTexturreHandle_ = 0;*/
+	//スプライト
+	Sprite* sprite_;
+	//3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+	//サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+	//デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
+	
 
 	/// <summary>
 	/// ゲームシーン用
